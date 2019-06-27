7d25da044458927bb9cb8480d7a9000b986e9412
constantino perez jimenez <cossystem@hotmail.com>
2019-06-27 15:36:52 -0500

    Creacion del Proyecto

---
+++ CursoBasico/CursoBasico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoBasico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
