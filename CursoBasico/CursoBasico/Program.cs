2b09639ca88a9d7442393aa146d2d0279a4e5197
constantino perez jimenez <cossystem@hotmail.com>
2019-06-27 15:38:02 -0500

    Se Agrego una Nueva Linea

--- CursoBasico/CursoBasico/Program.cs
+++ CursoBasico/CursoBasico/Program.cs
@@ -7,6 +7,8 @@ namespace CursoBasico
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Esto es Una Prueba");
+
         }
     }
 }
