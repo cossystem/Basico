27ed101ea4f6aa1d95cde3e8c0837faa82b8ae4c
constantino perez jimenez <cossystem@hotmail.com>
2019-06-27 21:12:03 -0500

    Orderby y ThenBy

--- CursoBasico/LINQ13/Program.cs
+++ CursoBasico/LINQ13/Program.cs
@@ -10,6 +10,7 @@ namespace LINQ13
         {
             string[] postres = { "pay de manzana", "pastel de chocolate", "manzana caramelada", "fresa con crema", "pay de pera y manzana" };
             string[] palabras = { "hola", "piedra", "pato", "pastel", "carros", "auto" };
+            int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             List<CEstudiante> estudiantes = new List<CEstudiante>
             {
@@ -62,15 +63,54 @@ namespace LINQ13
 
             //Zip 
             //Regresa una secuencia que aplica un funcion a cada par
-
-            Console.WriteLine("-----zip-----\r\n");
             #region zip
+            Console.WriteLine("-----zip-----\r\n");
+
             string[] helados = { "Chocolate", "vainilla", "fresa", "limon" };
 
-            IEnumerable<string> r12 = postres.zip()
+            IEnumerable<string> r12 = postres.Zip(helados, (p, h) => p + " con helado de " + h);
 
+            foreach (var lst in r12)
+            {
+                Console.WriteLine(lst);
+            }
 
             #endregion
+
+            ///Ordenamiento
+            ///Orderby Theby Ordena en orden ascendente
+            ///OrderbyDescending, ThenByDescending Ordena en orden descendente
+            ///Reverse Regresa en el orden inverso
+
+            #region Orderby
+            Console.WriteLine("------Ordenamiento ---->\r\n");
+
+            IEnumerable<int> numOrder = numeros.OrderBy(n => n);
+            IEnumerable<int> numDes = numeros.OrderByDescending(n => n);
+
+            //ordenar pór la longitud de la palagra y despues alfaberico
+            IEnumerable<string> palabrao = palabras.OrderBy(n => n.Length).ThenBy(p => p) ;
+
+            foreach (var lst1 in numOrder)
+            {
+                Console.WriteLine(lst1);
+            }
+
+            Console.WriteLine("----Ordenamiento Descendiente----\r\n");
+
+            foreach (var lst2 in numDes)
+            {
+                Console.WriteLine(lst2);
+            }
+
+            foreach (var lst3 in palabrao)
+            {
+                Console.WriteLine(lst3);
+            }
+            #endregion
+
+
+
         }
     }
 }
